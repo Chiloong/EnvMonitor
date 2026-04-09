9ce2ce1083658e4f1ba7e1436b4ceac80a01d390
Chiloong <76913920+Chiloong@users.noreply.github.com>
2026-04-09 11:01:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from wind import check_wind
-from pressure import check_pressure
-from fusion import check_fusion
+from fusion import check_all
 
 def main():
-    wind_data = check_wind()
-    pressure_data = check_pressure()
-    check_fusion(wind_data, pressure_data)
+    check_all()
 
 if __name__ == "__main__":
     main()
