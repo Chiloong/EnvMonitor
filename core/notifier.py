94675a5c3fa60ce57dc33a0054bc375194540b31
Chiloong <76913920+Chiloong@users.noreply.github.com>
2026-04-11 14:03:58 +0800

    Update notifier.py

--- core/notifier.py
+++ core/notifier.py
@@ -1,26 +1,26 @@
 import requests
 from config import BARK_KEY
-from urllib.parse import quote
 
 
 def send(msg):
     print("bark send")
 
     try:
-        # 🔥分离标题和内容（最多4行）
         lines = msg.split("\n")
 
         title = lines[0]
         body = "\n".join(lines[1:])
 
-        url = (
-            f"https://api.day.app/{BARK_KEY}/"
-            f"{quote(title)}"
-            f"?body={quote(body)}"
-        )
+        url = f"https://api.day.app/push"
 
-        r = requests.get(url, timeout=10)
-        print(r.status_code)
+        data = {
+            "device_key": BARK_KEY,
+            "title": title,
+            "body": body
+        }
+
+        r = requests.post(url, json=data, timeout=10)
+        print(r.status_code, r.text)
 
     except Exception as e:
         print("bark error", e)
